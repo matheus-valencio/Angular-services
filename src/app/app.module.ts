c40cf28251ea699c49575b87bc3ee55ada25c288
matheus-valencio <matheus.valencio@live.com>
2022-04-01 19:25:37 -0300

    2 files modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,30 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { LogComponent } from './log/log.component';
+import { RouterModule } from '@angular/router';
+import { TimerService } from './timer.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'tasks', component: TasksComponent },
+      { path: 'log', component: LogComponent },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HomeComponent,
+    TasksComponent,
+    LogComponent,
+  ],
+  bootstrap: [AppComponent],
+  providers: [TimerService],
 })
-export class AppModule { }
+export class AppModule {}
